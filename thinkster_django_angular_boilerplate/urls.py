8fad61a13ed8855065c2df9f5683a18550c06c0b
sohamnavadiya <soham@auberginesolutions.com>
2017-04-07 23:18:17 +0530

    Solving registration problem

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -13,14 +13,9 @@ router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
 
 urlpatterns = patterns(
-     '',
-    url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
-
-    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
-    # ... URLs
+    '',
     url(r'^api/v1/', include(router.urls)),
-
+    url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
     url('^.*$', IndexView.as_view(), name='index'),
-
-
 )
